554532949bd3bf3b839dc01669da7f8b989fb353
Mikhail Kornilov <korn-m@yandex-team.ru>
2023-03-31 18:05:29 +0300

    initial commit

---
+++ statistics/__init__.py
@@ -0,0 +1,18 @@
+"""
+Персональная статистика организации поставщика
+"""
+__author__ = 'Python 3.6 mafia'
+__maintainer__ = __author__
+
+__email__ = 'm2005052@edu.misis.ru'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
